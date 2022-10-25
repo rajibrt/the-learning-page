54741cc19cb96a50dfd5cb3fd26e21dd1d7192a4
rajibrt <rajibrt@gmail.com>
2022-10-26 05:56:08 +0600

    course desing updated

--- src/Pages/CourseEnroll.js
+++ src/Pages/CourseEnroll.js
@@ -11,7 +11,7 @@ const CourseEnroll = () => {
 
         <div className="flex items-center justify-center relative" >
             <div className="md:w-8/12 w-full rounded-lg shadow-2xl overflow-hidden lg:mx-0 md:my-8 mx-8 md:mx-0">
-                <img className="absolute inset-0 h-full w-full object-cover" src={picture} alt="" />
+                <img className="absolute inset-0 h-full w-full object-cover blur-sm " src={picture} alt="" />
                 <div className="absolute inset-0 bg-gray-900 bg-opacity-75"></div>
                 <div className="items-center relative mt-8">
                     <div className="relative">
@@ -19,15 +19,15 @@ const CourseEnroll = () => {
                         {/* <img className="rounded-t-lg" src={picture} alt="course" /> */}
 
                         <div className="relative lg:h-[700px] md:h-[450px] h-64 bg-center bg-cover rounded-t-md" style={{ backgroundImage: `url("${picture}")` }}>
-                            <p className='absolute right-4 top-4 bg-white/50 rounded-md px-2 text-2xl font-bold text-blue-600'>$ {price} </p>
-                            <div className='absolute bottom-11 shadow-md bg-slate-100/50 p-4 w-full shadow-md'>
-                                <p className="capitalize font-bold"><span className="font-bold"><kbd className="kbd">Author: </kbd></span> {created_by}</p>
+                            <h2 className='text-4xl w-full p-2 absolute top-0 bg-black/50 text-white'>{title}</h2>
+                            <div className='absolute bottom-0 shadow-md bg-slate-100/50 p-4 w-full'>
+                                <p className="capitalize font-bold"><span className="font-bold">Author:</span> {created_by}</p>
                                 <p className="btn btn-ghost capitalize"><FaClock className="mr-2 text-white "></FaClock>{course_duration}</p>
                                 <p className="btn btn-ghost capitalize"><FaBook className="mr-2 text-white "></FaBook>{total_module}</p>
 
+                                <p className='absolute right-4 top-4 bg-white/50 rounded-md px-2 text-2xl font-bold text-yellow-700'>$ {price} </p>
                             </div>
 
-                            <h2 className='card-title w-full p-2 absolute bottom-0 bg-black/50 text-white'>{title}</h2>
                         </div>
                         <div className="items-center md:p-8 grid gap-2 bg-white content-between">
                             <div className='m-4'>
@@ -38,7 +38,7 @@ const CourseEnroll = () => {
 
                                 <div className='flex justify-between gap-2'>
                                     <Link to='/'><button className="btn outline-0 bg-blue-800 text-white" ><FaArrowLeft></FaArrowLeft> </button></Link>
-                                    <button className="btn outline-0 bg-blue-800 text-white">Book Now</button>
+                                    <button className="btn outline-0 bg-blue-800 text-white">Enroll Now</button>
                                 </div>
                             </div>
                         </div>
