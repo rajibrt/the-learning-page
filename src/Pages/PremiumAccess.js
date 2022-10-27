c61d1875bc3bed13bf5a954260d7203ebc16c7d9
rajibrt <rajibrt@gmail.com>
2022-10-27 08:00:34 +0600

    probile name & theme icon updated

--- src/Pages/PremiumAccess.js
+++ src/Pages/PremiumAccess.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const PremiumAccess = () => {
     return (
-        <div>
-            <h2>Payment Page</h2>
+        <div className="h-screen flex items-stretch">
+            <div className="card w-3/5 self-center m-auto lg:card-side bg-base-100 shadow-xl">
+                <figure><img src="https://placeimg.com/400/400/arch" alt="Album" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">New album is released!</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Listen</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
